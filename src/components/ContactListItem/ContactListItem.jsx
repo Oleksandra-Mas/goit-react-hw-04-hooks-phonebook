5815835574e4172b583ff667829e7c8195c18d5b
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2021-12-26 23:57:24 +0200

    changed contactListItem from class component to function

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,37 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import styles from './ContactListItem.module.scss';
-class ContactListItem extends Component {
-    static propTypes = {
-        contact: PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            number: PropTypes.string.isRequired,
-        }).isRequired,
-        onDelete: PropTypes.func.isRequired,
-    };
-    state = {
-        name: '',
-        number: '',
-    };
-    render() {
-        const { contact, onDelete } = this.props;
-        return (
-            <li className={styles.item}>
-                <p>{contact.name}</p>
-                <a href={'tel:' + contact.number}>{contact.number}</a>
-                <Button
-                    variant="outline-dark"
-                    type="button"
-                    onClick={() => {
-                        onDelete(contact.id);
-                    }}
-                >
-                    Delete
-                </Button>
-            </li>
-        );
-    }
+export default function ContactListItem({ contact, onDelete }) {
+    return (
+        <li className={styles.item}>
+            <p>{contact.name}</p>
+            <a href={'tel:' + contact.number}>{contact.number}</a>
+            <Button
+                variant="outline-dark"
+                type="button"
+                onClick={() => {
+                    onDelete(contact.id);
+                }}
+            >
+                Delete
+            </Button>
+        </li>
+    );
 }
-export default ContactListItem;
+ContactListItem.propTypes = {
+    contact: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+    }).isRequired,
+    onDelete: PropTypes.func.isRequired,
+};
